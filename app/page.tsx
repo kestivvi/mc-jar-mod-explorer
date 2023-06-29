43085168c370162703255072aa04d888b585181d
Krzysztof Kwiatkowski <kestivvi@gmail.com>
2023-06-29 19:53:27 +0200

    Refactor home page

--- app/page.tsx
+++ app/page.tsx
@@ -6,22 +6,27 @@ import ProgressBar from './(root)/ProgressBar';
 import ErrorComponent from './(components)/ErrorComponent';
 
 export default function Home() {
-    const [progressPercent, setProgressPercent] = useState<number>(0);
-    const [error, setError] = useState<string>("");
-    const [isSending, setIsSending] = useState<boolean>(false);
+    const [progressPercent, setProgressPercent] = useState(0);
+    const [error, setError] = useState('');
+    const [isSending, setIsSending] = useState(false);
 
-    function onFileSubmit(file: File) {
+    async function onFileSubmit(file: File) {
         const formData = new FormData();
         formData.append('file', file);
 
-        var xhr = new XMLHttpRequest();
-        xhr.open("POST", "/api/upload");
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', '/api/upload');
 
-        xhr.onload = async function () {
+        xhr.upload.onprogress = (event: ProgressEvent) => {
+            const percent = (event.loaded / event.total) * 100;
+            setProgressPercent(percent);
+        };
+
+        xhr.onload = async () => {
             setIsSending(false);
 
             if (xhr.status === 200) {
-                console.log("Upload successful!");
+                console.log('Upload successful!');
                 setError('');
 
                 const json = JSON.parse(xhr.responseText);
@@ -29,33 +34,27 @@ export default function Home() {
 
                 window.location.href = `/${hash}`;
             } else {
-                console.error("Error uploading files. Please try again.");
+                console.error('Error uploading files. Please try again.');
                 setError('Error uploading files. Please try again.');
             }
-        };
-
-        xhr.upload.onprogress = function (event) {
-            var percent = (event.loaded / event.total) * 100;
-            setProgressPercent(percent);
         };
 
         setIsSending(true);
         xhr.send(formData);
     }
+
+
     return (
         <div className='m-20 flex flex-col justify-center items-center gap-12'>
-
             <div className='flex justify-center items-center text-lg'>
-                This website tool enables you to analyze unknown minecraft mod just by uploading its jar file.
+                This website tool enables you to analyze an unknown Minecraft mod by uploading its JAR file.
             </div>
 
             <FileUpload onFileSubmit={onFileSubmit} />
 
-            <div className={isSending ? "" : "hidden"}>
-                <ProgressBar value={progressPercent} />
-            </div>
+            {isSending && <ProgressBar value={progressPercent} />}
 
-            <ErrorComponent error={error} />
+            {error && <ErrorComponent error={error} />}
         </div>
-    )
+    );
 }
